8481dff762615c07964a557ccc59bdf856978788
SaraleePam <109116723+SaraleePam@users.noreply.github.com>
2023-04-11 22:53:26 -0400

    Update RasPi_DayNight_expiriment.py

--- RasPi_DayNight_expiriment.py
+++ RasPi_DayNight_expiriment.py
@@ -20,21 +20,44 @@ def Dayleng_Angle():
     lon = -73.962570
     API_key = '38ca058a77bcc7e906c1c6adb7e49e35'
     url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}'
+    url = url.format(lat=lat, lon=lon, API_key=API_key)
+    print(url)
     r = requests.get(url)
     r.status_code
+    pp(r.json())
     Sunrise = r.json()['sys']['sunrise']
     Sunset = r.json()['sys']['sunset']
     DaylengUnix = Sunset - Sunrise
     pp(DaylengUnix)
-    while sec < DaylengUnix:
-        print(sec)
-        sec += 1
+    #while sec < DaylengUnix:
+    #    print(sec)
+    #    sec += 1
     return (sec/DaylengUnix)*180
 
 
 while True:
 
-    time.sleep()
+    time.sleep(1)
     stepangle = int(Dayleng_Angle()  /360.0 * 800.0)
     ser.write(('DAY_MOVE ' + str(stepangle) + '\n').encode())
+    
+    
+    
+"""
+
+last_update = 0
+dayleng = 0
+
+while True:
+
+
+    if current_time - last_update > 24h:
+        update_Dayleng()
+        current_time = now
+    
+    rotate_motor_to_current_angle()
+    
+    time.sleep(60)
+    
+"""
     
