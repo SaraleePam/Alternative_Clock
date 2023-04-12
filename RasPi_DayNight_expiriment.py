52b45169dc45bc9a417f5ab99db3c5ab62bc9226
SaraleePam <109116723+SaraleePam@users.noreply.github.com>
2023-04-12 15:26:23 -0400

    2023 Apr 12

--- RasPi_DayNight_expiriment.py
+++ RasPi_DayNight_expiriment.py
@@ -4,6 +4,8 @@ import datetime
 import requests
 from pprint import pprint as pp
 
+microsteps = 16
+sec = 0
 
 #########################
 
@@ -13,8 +15,21 @@ time.sleep(3)
 ser.reset_input_buffer()
 
 
+def Sunrise():
+    sec = 0
+    lat = 40.807537
+    lon = -73.962570
+    API_key = '38ca058a77bcc7e906c1c6adb7e49e35'
+    url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}'
+    url = url.format(lat=lat, lon=lon, API_key=API_key)
+    print(url)
+    r = requests.get(url)
+    # r.status_code
+    pp(r.json())
+    Sunrise = r.json()['sys']['sunrise']
+    return (Sunrise)
 
-def Dayleng_Angle():
+def Sunset():
     sec = 0
     lat = 40.807537
     lon = -73.962570
@@ -25,39 +40,49 @@ def Dayleng_Angle():
     r = requests.get(url)
     r.status_code
     pp(r.json())
-    Sunrise = r.json()['sys']['sunrise']
-    Sunset = r.json()AccelStepper stepper1(AccelStepper::FULL2WIRE, 2, 5);['sys']['sunset']
-    DaylengUnix = Sunset - Sunrise
-    pp(DaylengUnix)
-    #while sec < DaylengUnix:
-    #    print(sec)
-    #    sec += 1
-    return (sec/DaylengUnix)*180
-
+    Sunset = r.json()['sys']['sunset']
+    return (Sunset)
 
 while True:
 
-    time.sleep(1)
-    stepangle = int(Dayleng_Angle()  /360.0 * 800.0)
-    ser.write(('DAY_MOVE ' + str(stepangle) + '\n').encode())
-    
-    
-    
-"""
+    if sec <= Sunrise():??
+        stepangle = int(sec /Dayleng() * 400.0 * microsteps)
 
+    else:
+        stepangle = int((sec /Nightleng() * 400.0 * microsteps) + (400 * microsteps))
+
+    sec += 1
+    print(sec)
+
+    ser.write(('DAY_MOVE ' + str(stepangle) + '\n').encode())
+    time.sleep(3)
+
+
+
+"""
 last_update = 0
 dayleng = 0
 
 while True:
 
-
     if current_time - last_update > 24h:
         update_Dayleng()
         current_time = now
-    
+
+    # while sec < DaylengUnix:
+    #    print(sec)
+    #    sec += 1
+
     rotate_motor_to_current_angle()
+    ser.write(('DAY_MOVE ' + str(stepangle) + '\n').encode())
     
     time.sleep(60)
     
+    while True:
+
+    time.sleep(1)
+    stepangle = int(Dayleng_Angle()  /360.0 * 800.0)
+    ser.write(('DAY_MOVE ' + str(stepangle) + '\n').encode())
 """
     
+
