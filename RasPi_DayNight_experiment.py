a9d90c58b35391e0f567de956ede74eb229976d8
SaraleePam <109116723+SaraleePam@users.noreply.github.com>
2023-04-20 21:23:26 -0400

    2023 Apr 20
    update day night

--- RasPi_DayNight_experiment.py
+++ RasPi_DayNight_experiment.py
@@ -2,7 +2,7 @@ import serial
 import time
 import datetime
 import requests
-from datetime import datetime, timedelta
+from datetime import  timedelta
 from pprint import pprint as pp
 
 microsteps = 16
@@ -10,6 +10,7 @@ lat = 40.807537
 lon = -73.962570
 today = datetime.date.today()
 tomorrow = today + timedelta(days=1)
+yesterday = today + timedelta(days=-1)
 current_time_unix = int(time.time())
 
 #########################################
@@ -36,10 +37,10 @@ url2 = url.format(lat=lat, lon=lon, date=tomorrow)
 r_tomorrow = requests.get(url2)
 next_sunrise = r_tomorrow.json()['results']['sunrise']
 
-next_sunrise_obj = datetime.datetime.strptime(next_sunrise, "%I:%M:%S %p")
-next_sunrise_datetime_obj = datetime.datetime.combine(tomorrow, next_sunrise_obj.time())
-next_sunrise_unix_time = next_sunrise_datetime_obj.timestamp()
 
+url3 = url.format(lat=lat, lon=lon, date=yesterday)
+r_yesterday = requests.get(url3)
+last_sunset = r_yesterday.json()['results']['sunset']
 
 sunrise_obj = datetime.datetime.strptime(sunrise, "%I:%M:%S %p")
 sunrise_datetime_obj = datetime.datetime.combine(today, sunrise_obj.time())
@@ -48,6 +49,15 @@ sunrise_unix_time = sunrise_datetime_obj.timestamp()
 sunset_obj = datetime.datetime.strptime(sunset, "%I:%M:%S %p")
 sunset_datetime_obj = datetime.datetime.combine(today, sunset_obj.time())
 sunset_unix_time = sunset_datetime_obj.timestamp()
+
+next_sunrise_obj = datetime.datetime.strptime(next_sunrise, "%I:%M:%S %p")
+next_sunrise_datetime_obj = datetime.datetime.combine(tomorrow, next_sunrise_obj.time())
+next_sunrise_unix_time = next_sunrise_datetime_obj.timestamp()
+
+
+last_sunset_obj = datetime.datetime.strptime(last_sunset, "%I:%M:%S %p")
+last_sunset_datetime_obj = datetime.datetime.combine(yesterday, last_sunset_obj.time())
+last_sunset_unix_time = last_sunset_datetime_obj.timestamp()
 
 
 def get_daylenght():
@@ -59,23 +69,29 @@ def get_nightlenght():
     nightlenght = next_sunrise_unix_time - sunset_unix_time
     return nightlenght
 
+
+def get_step_angle():
+    if sunrise_unix_time <= current_time_unix <= sunset_unix_time:
+        stepangle = int((current_time_unix-sunrise_unix_time) / get_daylenght() * 400.0 * microsteps)
+
+   #for night time after midnight (the today sunset&sunrise is not the same)
+    if (current_time_unix < sunrise_unix_time) and (current_time_unix < sunset_unix_time):
+        stepangle = int(((current_time_unix - last_sunset_unix_time) / get_nightlenght() * 400.0 * microsteps) + (400 * microsteps))
+
+    else:
+        stepangle = int(((current_time_unix - sunset_unix_time) / get_nightlenght() * 400.0 * microsteps) + (400 * microsteps))
+
+    return(stepangle)
+
 #########################################
+
 
 
 while True:
 
-    if sunrise_unix_time <= current_time_unix <= sunset_unix_time:
-        stepangle = int((current_time_unix-sunrise_unix_time) / get_daylenght() * 400.0 * microsteps)
+    ser.write(('SUN_MOVE ' + str(get_step_angle()) + '\n').encode())
+    time.sleep(5)
 
-    if (current_time_unix < sunset_unix_time) and (sunset_unix_time <= next_sunrise_unix_time):
-        stepangle = int((current_time_unix - sunset_unix_time / get_nightlenght() * 400.0 * microsteps) + (400 * microsteps))
-
-    else:
-        stepangle = int((current_time_unix - sunset_unix_time / get_nightlenght() * 400.0 * microsteps) + (400 * microsteps))
-
-
-    serial.write(('SUN_MOVE ' + str(stepangle) + '\n').encode())
-    time.sleep(1)
 
 #########################################
 
